a823621aa3bf43ad3655188b80d0a629fc801344
Huw Grano <Huw.Grano@cba.com.au>
2019-11-21 16:59:54 +1100

    adding value comparison methods

--- core/src/main/scala/commbank/grimlock/framework/Value2.scala
+++ core/src/main/scala/commbank/grimlock/framework/Value2.scala
@@ -14,6 +14,9 @@
 
 package commbank.grimlock.framework.encoding2
 
+import scala.reflect.ClassTag
+import scala.util.matching.Regex
+
 /** Trait for variable values. */
 trait Value[T] {
   /** The schema used to validate and encode/decode this value. */
@@ -24,13 +27,129 @@ trait Value[T] {
 
   if (!schema.validate(value))
     throw new IllegalArgumentException(s"${value} does not conform to schema ${schema.toShortString}")
+
+  /** Return value as `X` (if an appropriate converter exists), or `None` if the conversion is not supported. */
+  def as[X : ClassTag]: Option[X] = {
+    val ct = implicitly[ClassTag[X]]
+
+    def cast(a: Any): Option[X] = a match {
+      case ct(x) => Option(x)
+      case _ => None
+    }
+
+    schema
+      .converters
+      .flatMap { case convert => cast(convert(value)) }
+      .headOption
+      .orElse(cast(value))
+  }
+
+  /**
+   * Compare this value with a `T`.
+   *
+   * @param that The value to compare against.
+   *
+   * @return The returned value is < 0 iff this < that, 0 iff this = that, > 0 iff this > that.
+   */
+  def cmp(that: T): Int = schema.compare(value, that)
+
+  /**
+   * Compare this value with another.
+   *
+   * @param that The `Value` to compare against.
+   *
+   * @return If that can be compared to this, then an `Option` where the value is < 0 iff this < that,
+   *         0 iff this = that, > 0 iff this > that. A `None` in all other case.
+   */
+  def cmp(that: Value[_]): Option[Int]
+
+  /**
+   * Check for equality with `that`.
+   *
+   * @param that Value to compare against.
+   */
+  def equ(that: Value[_]): Boolean = evaluate(that, Equal)
+
+  /**
+   * Check if `this` is greater or equal to `that`.
+   *
+   * @param that Value to compare against.
+   *
+   * @note If `that` is of a type that can't be compares to `this`, then the result is always `false`. This
+   *       is the desired behaviour for the `Matrix.which` method (i.e. a filter).
+   */
+  def geq(that: Value[_]): Boolean = evaluate(that, GreaterEqual)
+
+  /**
+   * Check if `this` is greater than `that`.
+   *
+   * @param that Value to compare against.
+   *
+   * @note If `that` is of a type that can't be compares to `this`, then the result is always `false`. This
+   *       is the desired behaviour for the `Matrix.which` method (i.e. a filter).
+   */
+  def gtr(that: Value[_]): Boolean = evaluate(that, Greater)
+
+  /**
+   * Check if `this` is less or equal to `that`.
+   *
+   * @param that Value to compare against.
+   *
+   * @note If `that` is of a type that can't be compares to `this`, then the result is always `false`. This
+   *       is the desired behaviour for the `Matrix.which` method (i.e. a filter).
+   */
+  def leq(that: Value[_]): Boolean = evaluate(that, LessEqual)
+
+  /**
+   * Check for for match with `that` regular expression.
+   *
+   * @param that Regular expression to match against.
+   *
+   * @note This always applies `toShortString` method before matching.
+   */
+  def like(that: Regex): Boolean = that.pattern.matcher(this.toShortString).matches
+
+  /**
+   * Check if `this` is less than `that`.
+   *
+   * @param that Value to compare against.
+   *
+   * @note If `that` is of a type that can't be compares to `this`, then the result is always `false`. This
+   *       is the desired behaviour for the `Matrix.which` method (i.e. a filter).
+   */
+  def lss(that: Value[_]): Boolean = evaluate(that, Less)
+
+  /**
+   * Check for in-equality with `that`.
+   *
+   * @param that Value to compare against.
+   */
+  def neq(that: Value[_]): Boolean = !(this equ that)
+
+  /** Return a consise (terse) string representation of a value. */
+  def toShortString: String = schema.encode(value)
+
+  private def evaluate(that: Value[_], op: CompareResult): Boolean = cmp(that) match {
+    case Some(0) => (op == Equal) || (op == GreaterEqual) || (op == LessEqual)
+    case Some(x) if (x > 0) => (op == Greater) || (op == GreaterEqual)
+    case Some(x) if (x < 0) => (op == Less) || (op == LessEqual)
+    case _ => false
+  }
 }
 
 case class DecimalValue(value: BigDecimal, schema: Schema[BigDecimal]) extends Value[BigDecimal] {
-  // TODO add comparison methods
+  def cmp(that: Value[_]): Option[Int] = that.as[BigDecimal].map(bd => cmp(bd))
 }
 
 case class DoubleValue(value: Double, schema: Schema[Double]) extends Value[Double] {
-  // TODO add comparison methods
+  def cmp(that: Value[_]): Option[Int] = that.as[Double].map(d => cmp(d))
 }
+
+/** Hetrogeneous comparison results. */
+sealed private trait CompareResult
+private case object GreaterEqual extends CompareResult
+private case object Greater extends CompareResult
+private case object Equal extends CompareResult
+private case object Less extends CompareResult
+private case object LessEqual extends CompareResult
 
